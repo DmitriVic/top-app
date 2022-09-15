b53156f60d420e2735439a89a0f4f9acc143d7a5
unknown <mr_vlasov@mail.ru>
2022-09-15 12:07:50 +0300

    layout

---
+++ layout/layout.tsx
@@ -0,0 +1,28 @@
+// import s from './Button.module.css'
+// import { ButtonProps } from './Button.props'
+// import cn from 'classnames'
+// import ArrowIcon from './arrow.svg';
+
+// export const Button = ({ appearance, arrow = 'none',  children, className, ...props }: ButtonProps): JSX.Element => {
+// 	return (
+// 	<button  className={cn(s.button, className, {
+// 		[s.primary]: appearance == 'primary',
+// 		[s.ghost]: appearance == 'ghost',
+// 	})}
+// 	{...props}
+// 	>
+// 		{children}
+// 		{arrow != 'none' && <span className={cn(s.arrow, {
+// 				[s.down]: arrow == 'down'
+// 			})}>
+// 				<ArrowIcon />
+// 			</span>}
+			
+// 	</button>)
+// }
+
+
+export const шаблон = ({  }): JSX.Element => {
+	return <></>
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,34 @@
 
 
-import { Button, Htag, P, Tag } from '../components'
-
-
-
+import {   useState } from 'react'
+import { Button, Htag, P, Rating, Tag } from '../components'
 
 
 
 export default function Home (): JSX.Element  {
+	const [rating, setRating] = useState<number>(4);
+
+	
+
+
+	
+	
 
   return (
     < >
-   <Htag tag='h1'>Текст</Htag>
-	<Button appearance='primary' arrow='right'>Кнопка</Button>
-	<Button appearance='ghost' arrow='down' >Кнопка</Button>
-	<P size='s' >Lorem, ipsum dolor.</P>
-	<P size='m' >Lorem, ipsum dolor.</P>
-	<P size='l' >Lorem, ipsum dolor.</P>
-	<Tag color='primary' >Tag</Tag>
-	<Tag size='m' color='red' >Tag</Tag>
-	<Tag size='m' color='green'>Tag</Tag>
-	<Tag size='s' >Tag</Tag>
+   <div className='container'>
+		<Htag tag='h1'>Текст</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down' >Кнопка</Button>
+			<P size='s' >Lorem, ipsum dolor.</P>
+			<P size='m' >Lorem, ipsum dolor.</P>
+			<P size='l' >Lorem, ipsum dolor.</P>
+			<Tag color='primary' >Tag</Tag>
+			<Tag size='m' color='red' >Tag</Tag>
+			<Tag size='m' color='green'>Tag</Tag>
+			<Tag size='s' >Tag</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+	</div>
     </>
   )
 }
